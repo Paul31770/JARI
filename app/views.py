8e79fcfe43e144002dd83f9183f48f95d9b5ab0a
Paul31770 <paul.laffont31770@gmail.com>
2024-03-12 05:57:52 -0400

    Recup valeur de la bdd

--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from app.models import Item
 # Create your views here.
+def drag_drop(request):
+    Item.objects.create(name='CCS', column='column1')
+    Item.objects.create(name='TARZ', column='column2')
+
+    
+    all_items = Item.objects.all()
+    
+    print("all items ", all_items)
+    return render(request, 'drag.html',  context={'all_items': all_items})
